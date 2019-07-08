b12f3669e5388c71f01cdb7eb9e98dd6a7c92c61
Jeremy Drouillard <jadrouil@umich.edu>
2019-07-08 09:07:44 -0700

    Refactor logic into one class.
    -The logic was not complex enough to justify spreading across multiple
    files.
    -Use single method interfaces and lambda functions for practice.

--- src/main/java/jerm/famify/Famify.java
+++ src/main/java/jerm/famify/Famify.java
@@ -1,24 +1,64 @@
 package jerm.famify;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Famify {
     private String[] users;
-    private ICurator curator;
-    private IPlaylistController playlist;
+    private PlaylistOp playlistOps;
+    private UserTrackLookup userOps;
+    private final static int playlistSize = 20;
 
-    public Famify(String[] friends, ICurator cur, IPlaylistController con) {
+    public Famify(String[] friends, PlaylistOp playlist, UserTrackLookup userTracks) {
         users = friends;
-        curator = cur;
-        playlist = con;
+        playlistOps = playlist;
+        userOps = userTracks;
     }
 
     public void createOrUpdatePlaylist() {
         System.out.println("Selecting Songs From Friends: " + users.toString());
-        final List<String> tracks = curator.selectTracks(20, users);
-        if (!playlist.playlistExists()) {
-            playlist.createPlaylist();
+        List<String> selections = new ArrayList<>(playlistSize);
+
+        List<List<String>> crossUserCollection = new ArrayList<>(users.length);
+        for (String user: users) {
+            // 10 is a default. We don't ask for more for now.
+            List<String> playsByUser = userOps.getTracks(user, 10);
+            crossUserCollection.add(playsByUser);
         }
-        playlist.setPlaylist(tracks);
+
+        int head = 0;
+        int tail = 9;
+        boolean useHead = true;
+        int userIndex = 0;
+
+        while (selections.size() != playlistSize) {
+            String select;
+
+            int selectIndex = useHead ? head : tail;
+            select = crossUserCollection.get(userIndex++).get(selectIndex);
+            selections.add(select);
+
+            if (userIndex >= users.length) {
+                if (useHead) {
+                    head++;
+                }
+                else {
+                    tail--;
+                }
+                useHead = !useHead;
+                userIndex = 0;
+            }
+        }
+
+        playlistOps.create(users[0], selections);
     }
+
+    public interface PlaylistOp {
+        void create(String owner, List<String> tracks);
+    }
+
+    public interface UserTrackLookup {
+        List<String> getTracks(final String accessKey, final int numTracks);
+    }
+
 }
